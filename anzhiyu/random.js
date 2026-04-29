6e33b89f4433bfe4fd39c3a4fa0fe2f4551ff31f
F2skyline <1444652981@qq.com>
2026-04-29 20:28:27 +0800

    Site updated: 2026-04-29 20:28:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/28/hello-world/"];function toRandomPost(){
+var posts=["2026/04/29/TEXT/","2026/04/28/hello-world/","2026/04/29/你的文章标题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
